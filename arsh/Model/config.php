6cd8ab2eae312876fe59d2fb1d998bb343af45c0
Arshdeep Singh <boparai.arsh3@gmail.com>
2017-10-14 16:31:13 -0400

    Update config.php

--- arsh/Model/config.php
+++ arsh/Model/config.php
@@ -2,17 +2,17 @@
 include_once('databasename.php');
 
 $con = mysqli_connect($servername, $username, $password);
-$sql = "CREATE DATABASE IF NOT EXISTS login2";
+$sql = "CREATE DATABASE IF NOT EXISTS finalexam";
 $con->query($sql);
 mysqli_close($con);
 
 $con=mysqli_connect($servername, $username, $password, $dbname);
-$q="SHOW TABLES LIKE 'login'";
+$q="SHOW TABLES LIKE 'loginTable'";
 if($result=mysqli_query($con,$q))
 {
 	if($result->num_rows == 0) 
 	{
-		$sql = "CREATE TABLE login (
+		$sql = "CREATE TABLE loginTable (
           		Id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
            	 	UserName VARCHAR(30) NOT NULL,
             	Password VARCHAR(30) NOT NULL)";
@@ -24,15 +24,15 @@ mysqli_close($con);
 
 $con=mysqli_connect($servername, $username, $password, $dbname);
 
-$sql="select * from login";
+$sql="select * from loginTable";
 $result=mysqli_query($con,$sql);
 if($result->num_rows==0)
 {
-	$sql="insert into login values (null,'admin','admin@123')";
+	$sql="insert into loginTable values (null,'admin','admin@123')";
 	$con->query($sql);
-	$sql="insert into login values (null,'arsh','123')";
+	$sql="insert into loginTable values (null,'duttpatel','hellodutt')";
 	$con->query($sql);                   
-	$sql="insert into login values (null,'guri','1234')";
+	$sql="insert into loginTable values (null,'jeetpatel','hellojeet')";
 	$con->query($sql);                   
 	mysqli_close($con);  
 }
